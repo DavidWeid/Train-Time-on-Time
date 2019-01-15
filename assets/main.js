7bc887d599049fc294321dc42088651c7b05428d
David Weid <david.weid.2@gmail.com>
2019-01-15 17:15:35 -0500

    adding sign-in auth

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,31 @@ var config = {
 
 firebase.initializeApp(config);
 
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: "trainsMainPage.html",
+  signInOptions: [
+
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+
+  // tosUrl and privacyPolicyUrl accept either url string or a callbacK function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign("<your-privacy-policy-url>");
+  }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
+
 var database = firebase.database().ref("/trainInfo");
 
 // When user clicks "Submit"
